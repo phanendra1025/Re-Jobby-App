14fd17e87c86e962c7f2f9c7976f073ecf09562f
phanendra <phanendrakumar2510@gmail.com>
2023-06-29 11:23:26 +0000

    react jobby app commit message

--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -32,6 +32,7 @@ class JobItemDetails extends Component {
     const {match} = this.props
     const {params} = match
     const {id} = params
+    console.log(id)
     const jwtToken = Cookies.get('jwt_token')
     const url = `https://apis.ccbp.in/jobs/${id}`
     const options = {
